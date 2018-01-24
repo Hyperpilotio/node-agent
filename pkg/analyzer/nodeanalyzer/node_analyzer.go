059cf83dbb2af444b70232f9da4c35eb75c86b1c
alan.yang <jpra1113@gmail.com>
2018-01-24 11:16:34 +0800

    Replace intel docker wildcard calue easily

--- pkg/analyzer/nodeanalyzer/node_analyzer.go
+++ pkg/analyzer/nodeanalyzer/node_analyzer.go
@@ -16,6 +16,7 @@ import (
 )
 
 type NodeAnalyzer struct {
+	initialized       bool
 	DerivedMetrics    *DerivedMetrics
 	MetricPatterns    []glob.Glob
 	NormalizerMapping map[string]string
@@ -34,6 +35,72 @@ func NewAnalyzer() *NodeAnalyzer {
 	}
 }
 
+func (p *NodeAnalyzer) init(cfg snap.Config) error {
+	configs, ok := cfg["configs"]
+	if !ok {
+		return snap.ErrConfigNotFound
+	}
+
+	dmCfgs := []DerivedMetricConfig{}
+	for _, config := range configs.([]interface{}) {
+		bytes, err := json.Marshal(config)
+		if err != nil {
+			return fmt.Errorf("Unable to marshal src: %s", err)
+		}
+
+		dmCfg := DerivedMetricConfig{}
+		err = json.Unmarshal(bytes, &dmCfg)
+		if err != nil {
+			return fmt.Errorf("Unable to unmarshal into dst: %s", err)
+		}
+
+		if dmCfg.Normalizer != nil {
+			p.NormalizerMapping[dmCfg.MetricName] = *dmCfg.Normalizer
+		}
+		dmCfgs = append(dmCfgs, dmCfg)
+
+		pattern, err := glob.Compile(dmCfg.MetricName)
+		if err != nil {
+			return fmt.Errorf("Unable to compile pattern %s: %s", dmCfg.MetricName, err.Error())
+		}
+		p.MetricPatterns = append(p.MetricPatterns, pattern)
+	}
+
+	interval, err := cfg.GetString("sampleInterval")
+	if err != nil {
+		return errors.New("Unable to find sampleInterval duration: " + err.Error())
+	}
+
+	sampleInterval, err := time.ParseDuration(interval)
+	if err != nil {
+		return fmt.Errorf("Unable to parse %s to duration: %s", interval, err.Error())
+	}
+
+	derivedMetrics, err := NewDerivedMetrics(sampleInterval.Nanoseconds(), dmCfgs)
+	if err != nil {
+		return errors.New("Unable to create derived metrics: " + err.Error())
+	}
+
+	p.DerivedMetrics = derivedMetrics
+	p.initialized = true
+
+	return nil
+}
+
+func (p *NodeAnalyzer) replaceNamespaceWildcardValue(inputNm string, replaceNm string) string {
+	actualNms := []string{}
+	inputNms := strings.Split(inputNm, "/")
+	replaceNms := strings.Split(replaceNm, "/")
+	for i, element := range replaceNms {
+		if element == "*" {
+			element = inputNms[i]
+		}
+		actualNms = append(actualNms, element)
+	}
+
+	return strings.Join(actualNms, "/")
+}
+
 func (p *NodeAnalyzer) getNormalizerData(filterMetricNm string, filterNodename string, mts []snap.Metric) float64 {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
@@ -42,36 +109,12 @@ func (p *NodeAnalyzer) getNormalizerData(filterMetricNm string, filterNodename s
 	for _, mt := range mts {
 		mtMetricNm := "/" + strings.Join(mt.Namespace.Strings(), "/")
 		mtNodename := mt.Tags["nodename"]
-		dockerId := ""
-		networkId := ""
-		filesystemId := ""
-		if strings.HasPrefix(mtMetricNm, "/intel/docker") {
-			dockerId = mt.Namespace.Strings()[2]
-			if strings.Contains(mtMetricNm, "/stats/network/") {
-				networkId = mt.Namespace.Strings()[5]
-			}
-			if strings.Contains(mtMetricNm, "/stats/filesystem/") {
-				filesystemId = mt.Namespace.Strings()[5]
-			}
-		}
 
 		if mtMetricNm == filterMetricNm && mtNodename == filterNodename {
 			for metricName, normalizer := range p.NormalizerMapping {
-				if strings.HasPrefix(metricName, "/intel/docker") {
-					metricName = strings.Replace(metricName, "/intel/docker/*/",
-						fmt.Sprintf("/intel/docker/%s/", dockerId), 1)
-					normalizer = strings.Replace(normalizer, "/intel/docker/*/",
-						fmt.Sprintf("/intel/docker/%s/", dockerId), 1)
-
-					metricName = strings.Replace(metricName, "/stats/network/*/",
-						fmt.Sprintf("/stats/network/%s/", networkId), 1)
-					normalizer = strings.Replace(normalizer, "/stats/network/*/",
-						fmt.Sprintf("/stats/network/%s/", networkId), 1)
-
-					metricName = strings.Replace(metricName, "/stats/filesystem/*/",
-						fmt.Sprintf("/stats/filesystem/%s/", filesystemId), 1)
-					normalizer = strings.Replace(normalizer, "/stats/filesystem/*/",
-						fmt.Sprintf("/stats/filesystem/%s/", filesystemId), 1)
+				if strings.Contains(metricName, "*") {
+					metricName = p.replaceNamespaceWildcardValue(mtMetricNm, metricName)
+					normalizer = p.replaceNamespaceWildcardValue(mtMetricNm, normalizer)
 				}
 
 				if mtMetricNm == metricName {
@@ -159,53 +202,10 @@ func (p *NodeAnalyzer) Analyze(mts []snap.Metric, cfg snap.Config) ([]snap.Metri
 		return nil, errors.New("Unable to get metrics to analyze")
 	}
 
-	if p.DerivedMetrics == nil {
-		configs, ok := cfg["configs"]
-		if !ok {
-			return nil, snap.ErrConfigNotFound
+	if !p.initialized {
+		if err := p.init(cfg); err != nil {
+			return nil, err
 		}
-
-		dmCfgs := []DerivedMetricConfig{}
-		for _, config := range configs.([]interface{}) {
-			bytes, err := json.Marshal(config)
-			if err != nil {
-				return nil, fmt.Errorf("Unable to marshal src: %s", err)
-			}
-
-			dmCfg := DerivedMetricConfig{}
-			err = json.Unmarshal(bytes, &dmCfg)
-			if err != nil {
-				return nil, fmt.Errorf("Unable to unmarshal into dst: %s", err)
-			}
-
-			if dmCfg.Normalizer != nil {
-				p.NormalizerMapping[dmCfg.MetricName] = *dmCfg.Normalizer
-			}
-			dmCfgs = append(dmCfgs, dmCfg)
-
-			pattern, err := glob.Compile(dmCfg.MetricName)
-			if err != nil {
-				return nil, fmt.Errorf("Unable to compile pattern %s: %s", dmCfg.MetricName, err.Error())
-			}
-			p.MetricPatterns = append(p.MetricPatterns, pattern)
-		}
-
-		interval, err := cfg.GetString("sampleInterval")
-		if err != nil {
-			return nil, errors.New("Unable to find sampleInterval duration: " + err.Error())
-		}
-
-		sampleInterval, err := time.ParseDuration(interval)
-		if err != nil {
-			return nil, fmt.Errorf("Unable to parse %s to duration: %s", interval, err.Error())
-		}
-
-		derivedMetrics, err := NewDerivedMetrics(sampleInterval.Nanoseconds(), dmCfgs)
-		if err != nil {
-			return nil, errors.New("Unable to create derived metrics: " + err.Error())
-		}
-
-		p.DerivedMetrics = derivedMetrics
 	}
 
 	analyzeMts, err := p.getMetricTypes(mts)
